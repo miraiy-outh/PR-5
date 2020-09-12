3765b6390cc37dd2b7b9b53e19c7af3023eb8dd2
miraiy-outh <shokolatemeur@mail.ru>
2020-09-12 16:01:57 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class Main {
+    public static void main(String[] args) {
+        MovableCircle circle1 = new MovableCircle(1, 2, 5, 6, 10);
+        System.out.println(circle1.center.toString());
+    }
+}
---
+++ src/com/company/MovableCircle.java
@@ -0,0 +1,39 @@
+package com.company;
+
+public class MovableCircle implements Movable {
+    int radius;
+    MovablePoint center;
+
+    public MovableCircle(int x, int y, int xSpeed, int ySpeed, int radius) {
+        this.radius = radius;
+        this.center = new MovablePoint(x, y, xSpeed, ySpeed);
+    }
+
+    @Override
+    public String toString() {
+        return "MovableCircle{" +
+                "radius=" + radius +
+                ", center=" + center +
+                '}';
+    }
+
+    @Override
+    public void moveUp() {
+        center.y += center.ySpeed;
+    }
+
+    @Override
+    public void moveDown() {
+        center.y -= center.ySpeed;
+    }
+
+    @Override
+    public void moveLeft() {
+        center.x -= center.xSpeed;
+    }
+
+    @Override
+    public void moveRight() {
+        center.x += center.xSpeed;
+    }
+}
